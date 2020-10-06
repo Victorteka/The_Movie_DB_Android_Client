8699831146a21b549276270db07b271522fa7cf2
Victor Teka <ambukateka@gmail.com>
2020-10-06 05:28:42 +0300

    Setup viewpager

--- app/src/main/java/victorteka/github/io/tmdbapp/ui/movies/views/MovieFragment.kt
+++ app/src/main/java/victorteka/github/io/tmdbapp/ui/movies/views/MovieFragment.kt
@@ -1,11 +1,14 @@
 package victorteka.github.io.tmdbapp.ui.movies.views
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_movie.*
 import victorteka.github.io.tmdbapp.R
+import victorteka.github.io.tmdbapp.ui.movies.adapters.MovieViewPagerAdapter
 
 
 class MovieFragment : Fragment() {
@@ -16,5 +19,23 @@ class MovieFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
+
+        setupViewPager()
+    }
+
+    private fun setupViewPager() {
+        val adapter = activity?.supportFragmentManager?.let {
+            MovieViewPagerAdapter(supportFragmentManager = it)
+        }
+        adapter?.addFragment(UpcomingMoviesFragment(), "Upcoming")
+        adapter?.addFragment(LatestMoviesFragment(), "Latest")
+        adapter?.addFragment(PopularMoviesFragment(), "Popular")
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
     }
 }
--- app/src/main/java/victorteka/github/io/tmdbapp/ui/trendings/views/TrendingFragment.kt
+++ app/src/main/java/victorteka/github/io/tmdbapp/ui/trendings/views/TrendingFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_tranding.*
 import victorteka.github.io.tmdbapp.R
 
 
@@ -16,5 +18,10 @@ class TrendingFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tranding, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
     }
 }
